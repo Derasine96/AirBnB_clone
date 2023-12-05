80d1812a0b725fafd6503ec4e23b4932cf4d8238
Obikwelu Chidera <chidexobikwelu@gmail.com>
2023-12-04 16:10:45 -0800

    a method to generate a dictionary representation of an instance

--- models/base_model.py
+++ models/base_model.py
@@ -6,18 +6,27 @@ from datetime import datetime
 
 
 class BaseModel:
-
-    def __init__(self):
+    """Base class for AirBnb"""
+    def __init__(self, *args, **kwargs):
         """constructor method"""
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == "__class__":
+                    continue
+                setattr(self, key, value)
+            self.created_at = datetime.fromisoformat(self.created_at)
+            self.updated_at = datetime.fromisoformat(self.updated_at)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
     def __str__(self):
         """
         returns string representation of the object instance
         """
-        return "[{}] ({}) {}".format(type(self).__name__, self.id, self.__dict__)
+        return "[{}] ({}) {}".format(type(self).__name__,
+                                     self.id, self.__dict__)
 
     def save(self):
         """update instance attribute"""
